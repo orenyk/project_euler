59ae91d548fadd4178dda1dc0f9ffbcbdde38b5a
Oren Kanner <okanner@gmail.com>
2014-01-21 11:20:35 -0500

    problem 13

--- prob013.rb
+++ prob013.rb
@@ -20,7 +20,30 @@ class Prob13
 		puts s
 	end
 
+	def sum_of_nth_digit(n)
+		@nums.map { |num| (num / 10**(n-1)) % 10}.inject(&:+)
+	end
+
+	# pretty fast, 0.0001612 s
+	def brute_force
+		time do
+			sum = @nums.inject(&:+)
+			puts "sum: #{sum}"
+			num = sum
+			digs = []
+			while num > 9
+				num, last_digit = num.divmod(10)
+				digs << last_digit
+			end
+			digs << num
+			puts "The first 10 digits are #{digs.reverse[0..9].join}."
+		end
+	end
+
+
+
 end
 
 p = Prob13.new('prob013.txt')
 p.print_nums
+p.brute_force
